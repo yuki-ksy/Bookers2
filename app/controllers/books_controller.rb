1bf808c020da30987e4526b0ed99845a19641e05
EC2 Default User <ec2-user@ip-172-31-80-140.ec2.internal>
2020-09-30 06:44:08 +0000

    Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,7 +36,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to user_path
+    redirect_to books_path
   end
 
   def create
